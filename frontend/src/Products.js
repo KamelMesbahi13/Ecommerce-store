7a7fb60a33553defc7d2fab92cdde43de2458602
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-04-18 16:40:28 +0100

    adding products commit

---
+++ frontend/src/Products.js
@@ -0,0 +1,17 @@
+const product = [
+  {
+    _id: "1",
+    name: "Airpods Wireless Bluetooth Headphones",
+    image: "/images/airpods.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    brand: "Apple",
+    category: "Electronics",
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+];
+
+export default product;
