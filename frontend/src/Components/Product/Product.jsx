de42e56e992a07fcb29a6edbb8a4591f7641e4c5
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-04-18 19:29:06 +0100

    adding home screens commit

--- frontend/src/Components/Product/Product.jsx
+++ frontend/src/Components/Product/Product.jsx
@@ -1,5 +1,20 @@
-const Product = () => {
-  return <div>Product</div>;
+/* eslint-disable react/prop-types */
+const Product = ({ products }) => {
+  return (
+    <div>
+      <div>
+        <a href={`/product/${products._id}`}>
+          <img src={products.image} alt={products.name} />
+        </a>
+      </div>
+      <div>
+        <a href={`/product/${products._id}`}>
+          <h3>{products.name}</h3>
+        </a>{" "}
+        <div>${products.price}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
--- frontend/src/Screens/HomeScreens.jsx
+++ frontend/src/Screens/HomeScreens.jsx
@@ -1,17 +1,18 @@
 import Products from "../Products";
+import Product from "../Components/Product/Product";
 
 const HomeScreens = () => {
   return (
     <div>
       <div className="container">
-        <h1>Latest Products</h1>
-        <div className="grid grid-cols-1 gap-4 md:gap-12 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6">
+        <h1 className="my-12">Latest Products</h1>
+        <div className="grid grid-cols-1 gap-4 md:gap-12 md:grid-cols-2 lg:grid-cols-4">
           {Products.map((products) => {
             return (
               <div key={products._id}>
                 <div>
                   <div>
-                    <h3>{products.name}</h3>
+                    <Product products={products} />
                   </div>
                 </div>
               </div>
