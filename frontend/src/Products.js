ea5607c46d53fc5c2f4ed5dccb6a6c6c6a34b487
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-04-18 19:31:55 +0100

    adding home screens commit

--- frontend/src/Products.js
+++ frontend/src/Products.js
@@ -2,7 +2,7 @@ const product = [
   {
     _id: "1",
     name: "Airpods Wireless Bluetooth Headphones",
-    image: "./../public/images/airpods.jpg",
+    image: "./images/airpods.jpg",
     description:
       "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
     brand: "Apple",
@@ -16,7 +16,7 @@ const product = [
   {
     _id: "2",
     name: "iPhone 11 Pro 256go Memory",
-    image: "./../public/images/phone.jpg",
+    image: "./images/phone.jpg",
     description:
       "The iPhone 11 Pro is a top-tier smartphone by Apple, featuring a powerful A13 Bionic chip, advanced triple-camera setup, and a sleek design. It delivers exceptional performance, stunning photography, and an immersive user experience, making it a standout choice for tech enthusiasts.",
     brand: "Apple",
@@ -30,7 +30,7 @@ const product = [
   {
     _id: "3",
     name: "Cannon EOS 880 DSLR Camera",
-    image: "./../public/images/camera.jpg",
+    image: "./images/camera.jpg",
     description:
       "The Canon EOS 880 DSLR Camera is a high-quality camera known for its excellent performance, advanced features, and user-friendly design. With its powerful imaging capabilities, versatile shooting modes, and durable build, it's an ideal choice for photography enthusiasts and professionals alike.",
     brand: "Cannon",
@@ -44,7 +44,7 @@ const product = [
   {
     _id: "4",
     name: "Sony Playstation 5 White Edition",
-    image: "./../public/images/playstation.jpg",
+    image: "./images/playstation.jpg",
     description:
       "The PlayStation 5 (PS5) is Sony's latest gaming console, known for its powerful hardware, stunning graphics, and immersive gaming experience. Featuring high-speed SSD storage, 4K graphics, and advanced haptic feedback controllers, it offers seamless gameplay, fast loading times, and realistic visuals. With a wide range of exclusive titles and backward compatibility, the PS5 is a top choice for gamers looking for cutting-edge gaming technology.",
     brand: "Sony",
@@ -58,7 +58,7 @@ const product = [
   {
     _id: "5",
     name: "Logitech G-Series Gaming Mouse",
-    image: "./../public/images/mouse.jpg",
+    image: "./images/mouse.jpg",
     description:
       "The Logitech G-Series Gaming Mouse is a premium gaming accessory designed for gamers seeking precision, speed, and comfort. With customizable buttons, high DPI settings, and ergonomic design, it offers a competitive edge in gaming. Its responsive tracking and customizable RGB lighting make it a favorite among gamers for immersive gameplay experiences.",
     brand: "Logitech",
@@ -72,7 +72,7 @@ const product = [
   {
     _id: "6",
     name: "Amazon Echo Dot 3rd Generation",
-    image: "./../public/images/alexa.jpg",
+    image: "./images/alexa.jpg",
     description:
       "The Amazon Echo Dot 3rd Generation is a compact and smart speaker that integrates with Amazon's Alexa voice assistant. It offers hands-free convenience for tasks like playing music, controlling smart home devices, setting reminders, and accessing information. With its sleek design, improved sound quality, and affordable price, it's a popular choice for smart home enthusiasts looking to enhance their living spaces with voice-controlled technology.",
     brand: "Amazon",
