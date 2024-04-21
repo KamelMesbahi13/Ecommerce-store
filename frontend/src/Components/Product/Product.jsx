a6a1e742bdc82841f17d5bcc67b15cc68a8d8255
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-04-21 18:43:48 +0100

    adding home screens commit

--- frontend/src/Components/Product/Product.jsx
+++ frontend/src/Components/Product/Product.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 const Product = ({ products }) => {
   return (
-    <div className="p-4 border h-[20rem] border-grey shadow-md">
+    <div className="p-4 border h-[22rem] border-grey shadow-md">
       <div>
         <a href={`/product/${products._id}`}>
           <img src={products.image} alt={products.name} />
@@ -9,9 +9,9 @@ const Product = ({ products }) => {
       </div>
       <div>
         <a href={`/product/${products._id}`}>
-          <h3>{products.name}</h3>
+          <h3 className="mt-4 mb-2 font-bold">{products.name}</h3>
         </a>{" "}
-        <div>${products.price}</div>
+        <div className="italic font-bold">${products.price}</div>
       </div>
     </div>
   );
