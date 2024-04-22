84aedd4e269196cae5ab71f7a90ad7273c335594
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-04-22 12:49:05 +0100

    adding routing commit

--- frontend/src/Components/Product/Product.jsx
+++ frontend/src/Components/Product/Product.jsx
@@ -1,20 +1,31 @@
-/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
 const Product = ({ products }) => {
   return (
     <div className="p-4 border md:h-[23rem] lg:h-[20rem] shadow-md border-grey">
       <div>
-        <a href={`/product/${products._id}`}>
+        <Link to={`/product/${products._id}`}>
           <img src={products.image} alt={products.name} />
-        </a>
+        </Link>
       </div>
       <div>
-        <a href={`/product/${products._id}`}>
+        <Link to={`/product/${products._id}`}>
           <h3 className="mt-4 mb-2 font-bold">{products.name}</h3>
-        </a>{" "}
+        </Link>{" "}
         <div className="italic font-bold">${products.price}</div>
       </div>
     </div>
   );
+};
+
+Product.propTypes = {
+  products: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+  }).isRequired,
 };
 
 export default Product;
