a60bcac03456a9ae090d6d1add6d90b9a83db885
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-04-22 19:26:59 +0100

    adding product screen commit

--- frontend/src/Screens/ProductScreen.jsx
+++ frontend/src/Screens/ProductScreen.jsx
@@ -13,11 +13,10 @@ const ProductScreen = () => {
           <div>
             <div className="px-4 py-2 my-8 text-white duration-500 hover:bg-hoverColor bg-mainColor w-fit">
               <button>
-                <Link to="/" />
-                Go Back
+                <Link to="/">Go Back</Link>
               </button>
             </div>
-            <div className="flex flex-col md:flex-row">
+            <div className="flex flex-col justify-between md:flex-row">
               <div>
                 <img
                   className="w-[25rem]"
@@ -25,18 +24,31 @@ const ProductScreen = () => {
                   alt={product.name}
                 />
               </div>
-              <div className="md:ml-4">
+              <div className="ml-4">
                 <div className="mt-4 md:w-3/4">
                   <h1>{product.name}</h1>
                 </div>
-                <div className="py-4 my-4 md:my-8 border-y">
+                <div className="py-2 my-4 md:py-4 md:my-8">
                   <Rating
                     value={product.rating}
                     text={`${product.numReviews} Review`}
                   />
                 </div>
-                <div>
+                <div className="mb-8">
                   <h6 className="italic">Price: {product.price}</h6>
+                </div>
+              </div>
+              <div className="border md:shadow-lg md:px-6 md:py-6 border-grey h-fit">
+                <div className="flex items-center">
+                  <h6 className="mr-4 md:mr-12">Price:</h6>
+                  <h6 className="italic">${product.price}</h6>
+                </div>
+
+                <div className="flex items-center mt-6">
+                  <h6 className="mr-4 md:mr-12">Status:</h6>
+                  <h6 className="italic">
+                    {product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
+                  </h6>
                 </div>
               </div>
             </div>
