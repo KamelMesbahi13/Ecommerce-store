7dc64ec709638db7151ef5fc47de35d39d34d2d4
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-04-22 20:31:46 +0100

    adding the backend commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+import express from "express";
+
+const port = 5000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Api Is Coming..");
+});
+
+app.listen(port, () => {
+  console.log(`Server Running On Port ${port}`);
+});
