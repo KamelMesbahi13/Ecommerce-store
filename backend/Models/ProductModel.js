b6c046bdfa5b194cd8ae4714276d339db35b121f
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-04-23 18:32:11 +0100

    adding config schemas commit

---
+++ backend/Models/ProductModel.js
@@ -0,0 +1,54 @@
+import mongoose, { Schema } from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  image: {
+    type: String,
+    required: true,
+  },
+
+  brand: {
+    type: String,
+    required: true,
+  },
+
+  category: {
+    type: String,
+    required: true,
+  },
+
+  description: {
+    type: String,
+    required: true,
+  },
+
+  reviews: [reviewSchema],
+
+  rating: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+
+  numReviews: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+
+  price: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+
+  countInStock: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
