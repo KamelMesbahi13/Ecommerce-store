705675726a39d8ddc03877dc3a27ebd2f2c5b324
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-04-23 18:04:02 +0100

    adding mongoose and connect it commit

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -3,9 +3,11 @@ import mongoose from "mongoose";
 const connectDb = async () => {
   try {
     const connect = await mongoose.connect(process.env.MONGODB_URL);
-    console.log(`MongoDb Connect To ${connect}`);
+    console.log(`MongoDb Connect To ${connect.connection.host}`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
     process.exit(1);
   }
 };
+
+export default connectDb;
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@ import express from "express";
 import products from "./Data/Products.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDb from "./Config/db.js";
 
 dotenv.config();
 
 const port = process.env.PORT || 8000;
+connectDb();
 const app = express();
 app.use(cors());
 
