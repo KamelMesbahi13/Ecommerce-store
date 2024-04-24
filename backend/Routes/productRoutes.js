fa8f49212d42fb6f0170b8a31a20eade88b175ea
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-04-24 17:31:29 +0100

    get products from db commit

--- backend/Routes/productRoutes.js
+++ backend/Routes/productRoutes.js
@@ -1,8 +1,14 @@
-app.get("/api/products", (req, res) => {
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
   res.json(products);
 });
 
-app.get("/api/products/:id", (req, res) => {
+router.get("/:id", (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
+
+export default router;
