9fc461c6dfca1df2f7ddad73cf5a2bf152f6c05a
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-04-24 18:58:19 +0100

    get products from db commit

--- backend/Routes/productRoutes.js
+++ backend/Routes/productRoutes.js
@@ -7,21 +7,29 @@ const router = express.Router();
 router.get(
   "/",
   asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
+    try {
+      const products = await Product.find({});
+      res.json(products);
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
   })
 );
 
 router.get(
   "/:id",
   asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
+    try {
+      const product = await Product.findById(req.params.id);
 
-    if (product) {
-      res.json(product);
+      if (product) {
+        res.json(product);
+      } else {
+        res.status(404).json({ message: "Product not found" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Internal Server Error" });
     }
-
-    res.status(404).json({ message: "Product not found" });
   })
 );
 
