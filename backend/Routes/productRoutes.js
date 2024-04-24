2637eadfe8b90babec5f86bffe3760f6a2e24bb5
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-04-24 17:25:33 +0100

    get products from db commit

---
+++ backend/Routes/productRoutes.js
@@ -0,0 +1,8 @@
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
--- backend/server.js
+++ backend/server.js
@@ -15,15 +15,6 @@ app.get("/", (req, res) => {
   res.send("Api Is Coming..");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
-
 app.listen(port, () => {
   console.log(`Server Running On Port ${port}`);
 });
