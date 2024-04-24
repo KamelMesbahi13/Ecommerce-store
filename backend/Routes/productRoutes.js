be1dea20e3c6547346fb65488a7160ce1c6a8323
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-04-24 18:27:08 +0100

    get products from db commit

--- backend/Routes/productRoutes.js
+++ backend/Routes/productRoutes.js
@@ -1,15 +1,28 @@
 import express from "express";
-import products from "../Data/Products.js";
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../Models/ProductModel.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json(products);
-});
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
 
-router.get("/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      res.json(product);
+    }
+
+    res.status(404).json({ message: "Product not found" });
+  })
+);
 
 export default router;
