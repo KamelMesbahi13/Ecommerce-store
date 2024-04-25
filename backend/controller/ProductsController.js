226d226f032ea7b7501b4ecdd8e264f49e1cae74
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-04-25 10:28:45 +0100

    products controller commit

---
+++ backend/controller/ProductsController.js
@@ -0,0 +1,38 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../Models/ProductModel.js";
+
+// DESCRIPTION:  Fetch All Products
+// ROUTE:   GET /api/products
+// ACCESS:   Public
+
+const getProducts = asyncHandler(async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
+
+// DESCRIPTION:  Fetch Product By Id
+// ROUTE:   GET /api/product/:id
+// ACCESS:   Public
+
+const getProductById = asyncHandler(async (req, res) => {
+  asyncHandler(async (req, res) => {
+    try {
+      const product = await Product.findById(req.params.id);
+
+      if (product) {
+        res.json(product);
+      } else {
+        res.status(404);
+        throw new Error("Resources Not Found");
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  });
+});
+
+export { getProducts, getProductById };
