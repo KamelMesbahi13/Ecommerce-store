4185033362233be640e42bb39e357551201a24b2
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-04-29 12:35:48 +0100

    adding cart functionalite commit

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "../slices/apiSlices";
+import cartSliceReducer from "../slices/cartSlice";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    cart: cartSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
